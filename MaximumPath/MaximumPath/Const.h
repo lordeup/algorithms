f4f97a4ac07ddec238848365229a104e2ba89138
Andrey Kalanov <32957572+lordeup@users.noreply.github.com>
2022-05-19 11:34:50 +0300

    fixed error text

--- MaximumPath/MaximumPath/Const.h
+++ MaximumPath/MaximumPath/Const.h
@@ -1,5 +1,5 @@
 #pragma once
 #include <string>
 
-const std::string ERROR_INVALID_ARGUMENTS = "Invalid arguments count\nUsage: MaximumPath.exe <file name>";
+const std::string ERROR_INVALID_ARGUMENTS = "Invalid arguments count\nUsage: MaximumPath.exe <input file name> <output file name>";
 const std::string ERROR_FAILED_OPEN_FILE = "Failed to open for reading";
